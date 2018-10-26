b50783590359e40156166b0ea228fbdb84bb561f
mluqmanbukhori <44484883+mluqmanbukhori@users.noreply.github.com>
2018-10-26 08:21:24 +0700

    Create read_tcp_socket_data.py

---
+++ python/source/read_tcp_socket_data.py
@@ -0,0 +1,12 @@
+# this project can be practise on python programming
+# ==================================================
+# title : read data with tcp socket connection
+# author: muhammad luqman bukhori
+# date  : 26/11/2018 - 08.20 WIB
+# ==================================================
+
+import sys
+
+HOST = 'localhost'  #replace your localhost or TCP/IP on your connection
+PORT = 28097        #replace your port connection ect. 8080 / 80 / ... any
+

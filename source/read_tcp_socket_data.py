704d5e156fbed40f7859597423a849040a5915dd
mluqmanbukhori <44484883+mluqmanbukhori@users.noreply.github.com>
2018-10-26 08:47:50 +0700

    Update read_tcp_socket_data.py
    source code programming

--- source/read_tcp_socket_data.py
+++ source/read_tcp_socket_data.py
@@ -1,13 +1,26 @@
 # this project can be practise on python programming
-# ==================================================
-# title : read data with tcp socket connection
-# author: muhammad luqman bukhori
-# date  : 26/11/2018 - 08.20 WIB
-# ==================================================
+# read data character with tcp socket connection - for CLIENT
+#   author: muhammad luqman bukhori
+#   date  : 26/11/2018 - 08.20 WIB
+#   edit  : -
 
 import sys
 
+# define HOST and PORT servers
+HOST = 'host'   #replace your server localhost or TCP/IP on your connection
+PORT = 28097    #replace your port connection ect. 8080 / 80 / ... any
 
-HOST = 'localhost'  #replace your localhost or TCP/IP on your connection
-PORT = 28097        #replace your port connection ect. 8080 / 80 / ... any
+# lets connect with socket server connection
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((HOST, PORT))
 
+# looping programm, wait for data read
+while True:
+    data = sock.recv(1024)      # read data from server
+    out = data.decode('ascii')  # convert to ascii data
+    print (out)                 # print any character data
+    if out == 'Z'
+      break
+
+# close connection      
+sock.close()
